ed4afaa32256c896640c692c93c8fe7699e84ff8
Vinícius Borges <viniborg25@gmail.com>
2023-12-27 16:33:15 -0300

    Add infinite scroll and loading spinner

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, TextInput, View } from "react-native"
+import { ActivityIndicator, FlatList, Text, TextInput, View } from "react-native"
 import { MagnifyingGlass } from "phosphor-react-native"
 import { styles } from "./styles"
 import { useEffect, useState } from "react"
@@ -14,14 +14,23 @@ interface Movie {
 
 const Home = () => {
   const [discoveryMovies, setDiscoveryMovies] = useState<Movie[]>([])
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     loadData()
   }, [])
 
   const loadData = async () => {
-    const response = await api.get("/movie/popular")
-    setDiscoveryMovies(response.data.results)
+    setLoading(true)
+    const response = await api.get("/movie/popular", {
+      params: {
+        page,
+      },
+    })
+    setDiscoveryMovies([...discoveryMovies, ...response.data.results])
+    setPage(page + 1)
+    setLoading(false)
   }
 
   return (
@@ -42,8 +51,13 @@ const Home = () => {
             numColumns={3}
             renderItem={(item) => <CardMovies data={item.item} />}
             showsVerticalScrollIndicator={false}
-            contentContainerStyle={{ paddingBottom: 150 }}
+            contentContainerStyle={{
+              paddingBottom: 150,
+              justifyContent: "center",
+            }}
+            onEndReached={() => loadData()}
           />
+          {loading && <ActivityIndicator size={50} color="#0296e5" />}
         </View>
       </View>
     </>
